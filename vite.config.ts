d3b76342d16517cab050407cabe332ee3696b5dd
Ahsan Ali <ahsanbutt6165@gmail.com>
2024-11-20 18:12:42 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
 // https://vitejs.dev/config/
+dotenv.config()
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       "/api":{
-        target: "http://localhost:3000"
+        target: process.env.VITE_API_URL,
       }
     }
   }
